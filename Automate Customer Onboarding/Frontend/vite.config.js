3bf6db6be909c96692f4a37146819ae8fee4eaac
YojanPawade <pawadeyojan487@gmail.com>
2024-12-13 23:03:13 +0530

    third protect BASE_URL of ACO

--- Automate Customer Onboarding/Frontend/vite.config.js
+++ Automate Customer Onboarding/Frontend/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "https://automate-customer-onboarding.onrender.com",
+      "/api/": import.meta.env.VITE_BASE_URL,
     },
   },
 });
